9791c18b5fe26de03a0c5660ac97afd950f92467
Artur Komarov <artur.komarov@gmail.com>
2019-02-13 16:44:35 +0700

    Delete unnecessary files

--- migrate.rb
+++
@@ -1,9 +0,0 @@
-require 'mysql2'
-
-client = Mysql2::Client.new(host: 'localhost', username: 'root',
-                            database: 'old_fnvolga')
-client.query('SELECT * FROM Articles limit 1').each do |row|
-  puts row['Title']
-  puts row['Subtitle']
-  puts row['Text']
-end
