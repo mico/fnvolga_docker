80030ee75e8e96907b6072f82213c375ec7c8f92
Artur Komarov <artur.komarov@gmail.com>
2019-02-13 16:38:57 +0700

    Update project description

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# old_fn_volga_to_new
+# Developer environment for fn-volga.ru
+
+## How to run
+- Install docker and docker-compose (http://docker.com)
+- Fix paths for MAIN_DUMP and USERS_DUMP in ./run-server.sh
+- Put site files to ./htdocs dir
+- Run ```sh run-server.sh``` from console
--- run-server.sh
+++ run-server.sh
@@ -1,8 +1,11 @@
+MAIN_DUMP = ~fn_volga.sql
+USERS_DUMP = ./fs_user.sql
+
 echo "CREATE DATABASE IF NOT EXISTS fn_volga;" > dump.sql
 echo "CREATE DATABASE IF NOT EXISTS fn_volga_push;" >> dump.sql
 # need to execute it every mysql container run
 # echo "SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));" >> dump.sql
 echo "USE fn_volga;" >> dump.sql
-cat ~/fn-volga.sql >> dump.sql
-cat ./fs_user.sql >> dump.sql
+cat $MAIN_DUMP >> dump.sql
+cat $USERS_DUMP >> dump.sql
 docker-compose up
