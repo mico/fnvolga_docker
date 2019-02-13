848d6fdab30fb8fc695fb462d2476774b88a7692
Artur Komarov <artur.komarov@gmail.com>
2019-02-13 15:49:25 +0700

    fixes

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,7 @@ FROM php:7.3-apache
 
 RUN docker-php-ext-install mysqli
 RUN docker-php-ext-install pdo_mysql
+RUN docker-php-ext-install imagick
+RUN docker-php-ext-install gd
 RUN sed -ri -e '/<Directory \/var\/www\/>/,/<\/Directory>/ s/AllowOverride None/AllowOverride all/' /etc/apache2/apache2.conf
 RUN a2enmod rewrite
--- run-server.sh
+++ run-server.sh
@@ -1,20 +1,8 @@
-# TODO: convert to docker-compose to make db run before app instance
-# docker stop app
-# docker rm app
-# docker stop db
-# docker rm db
-# docker build -t 7.3-custom .
-# docker run -e MYSQL_ALLOW_EMPTY_PASSWORD=1 --name db mysql:5 &
-# # restore dump
-# # docker exec -i db mysql -e 'create database fn_volga'
-# # docker exec -i db mysql fn_volga < /Users/mico/Downloads/u0099096_gazned.sql
-# docker run -d -p 127.0.0.1:1080:80 --link db:db --name app -v "$PWD/htdocs":/var/www/html 7.3-custom
-
-# mysql shell
-# docker exec -it db mysql -p'my-secret-pw' fn_volga
-
 echo "CREATE DATABASE IF NOT EXISTS fn_volga;" > dump.sql
 echo "CREATE DATABASE IF NOT EXISTS fn_volga_push;" >> dump.sql
+# need to execute it every mysql container run
+# echo "SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));" >> dump.sql
 echo "USE fn_volga;" >> dump.sql
-cat /Users/mico/Downloads/u0099096_gazned.sql >> dump.sql
+cat ~/fn-volga.sql >> dump.sql
+cat ./fs_user.sql >> dump.sql
 docker-compose up
